11bbf02fa21ed78e5e6ff2c17d2538459a7bfbb0
options3224@gmail.com <options3224@gmail.com>
2023-03-19 21:29:30 +0900

    Echo 무한 반복

--- Socket/Client/Client.cs
+++ Socket/Client/Client.cs
@@ -16,18 +16,24 @@ internal class Client {
         IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("192.168.0.14"), 20000);
         clientSocket.Connect(endPoint);
 
-        // 서버로 전송할 문자열 입력
-        Console.Write(">> ");
-        string str = Console.ReadLine();
+        Console.WriteLine("서버로 전송할 문자열을 입력하세요.");
+        Console.WriteLine("(문자열 없이 [Enter] 입력하면 프로그램 종료)\n");
 
-        // 문자열 없이 [Enter] 입력 시 프로그램 종료
-        if (str == "")
-            return;
+        while (true) {
+            // 서버로 전송할 문자열 입력
+            Console.Write(">> ");
+            string str = Console.ReadLine();
 
-        // 직렬화: string 객체를 전송 가능한 byte 배열로 변환
-        byte[] buffer = Encoding.UTF8.GetBytes(str);
+            // 문자열 없이 [Enter] 입력 시 프로그램 종료
+            if (str == "") {
+                return;
+            }
 
-        // 서버로 데이터 전송
-        clientSocket.Send(buffer);
+            // 직렬화: string 객체를 전송 가능한 byte 배열로 변환
+            byte[] buffer = Encoding.UTF8.GetBytes(str);
+
+            // 서버로 데이터 전송
+            clientSocket.Send(buffer);
+        }
     }
 }
